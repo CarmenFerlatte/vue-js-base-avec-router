09b9858625bd7fce01bdf07ebd211dcd8fda845c
Carmen Ferlatte <carmen.ferlatte@gmail.com>
2024-12-06 15:51:15 -0500

    initialisation vue.js avec router

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/images/logo.png">
+        <h1>{{ message }}</h1>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Salut le monde !'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Accueil.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Accueil</h1>
+        <p>Bienvenue sur la page d'accueil !</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Accueil'
+}
+</script>
+
+<style scoped>
+
+</style>
